0fd00173f05c4816917798d25125c1153f7d292f
shabdar <2419038@vum.bg>
2026-05-10 22:47:09 +0300

    added custom CE favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className="bg-gray-50">
         <AuthProvider>
           <Navbar />
